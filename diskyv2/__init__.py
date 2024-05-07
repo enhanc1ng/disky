7bcfcad6446e22e2ddf99f9757153c98878f6387
stewiejunk <bytekey@zohomail.com>
2024-05-07 17:08:50 -0400

    Delete diskyv2 directory

--- diskyv2/__init__.py
+++
@@ -1,1 +0,0 @@
-from diskyv2.client import Client
--- diskyv2/errors.py
+++
@@ -1,9 +0,0 @@
-class MissingAuth(Exception):
-    def __init__(self, message="Token or both Email & Password are required!"):
-        self.message = message
-        super().__init__(self.message)
-
-class FailedAuth(Exception):
-    def __init__(self, message="Login was invalid!"):
-        self.message = message
-        super().__init__(self.message)
